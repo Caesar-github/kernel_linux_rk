2f75285df961d74de84412ca859a541b53b8fc55
chensq <chensq@rock-chips.com>
2021-04-07 16:08:32 +0800

    make for more android version
    Signed-off-by: chensq <chensq@rock-chips.com>

--- make-android.sh
+++ make-android.sh
@@ -4,9 +4,9 @@ JOB=`sed -n "N;/processor/p" /proc/cpuinfo|wc -l`
 
 function help()
 {
-	echo "Usage: ./make-android.sh ARCH BOOTIMG DTS"
+	echo "Usage: ./make-android.sh ARCH BOOTIMG DTS AndroidVersion"
 	echo ""
-	echo "e.g. ./make-android.sh arm64 ../rockdev/Image-rk3399pro-Android10 rk3399pro-toybrick-prod-android"
+	echo "e.g. ./make-android.sh arm64 ../rockdev/Image-rk3399pro-Android10 rk3399pro-toybrick-prod-android 10(or 11)"
 	echo ""
 }
 
@@ -15,5 +15,5 @@ if [ $# -lt 3 ];then
         exit 1
 fi
 
-make ARCH=$1 rockchip_defconfig android-10.config
+make ARCH=$1 rockchip_defconfig android-$4.config
 make ARCH=$1 BOOT_IMG=$2/boot.img $3.img -j${JOB}
